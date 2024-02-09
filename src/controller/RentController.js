b1cf9f37764a08239f4be0c172bf4a68b302bdb4
Santosh Pandit <panditsantosh19971@outlook.com>
2024-02-09 20:56:59 +0530

    edit

--- src/controller/RentController.js
+++ src/controller/RentController.js
@@ -6,92 +6,104 @@ const moment = require('moment');
 const data = {
     roomNumber: 1,
     generateDate: "15-01-2024",
-    billMonth:"December",
-    start:392,
-    end:396,
+    billMonth: "December",
+    start: 392,
+    end: 396,
     unit: 4,
-    totalBill:32,
+    totalBill: 32,
     roomRent: 1600,
     total: 1632,
-    prevAdvance:0,
-    prevBalance:1664,
-    grandtotal:3264,
-    paid:1600,
-    paidOn:"15-01-2024",
-    nextBalance:1664,
-    nextAdvance:0,
-    status:"paid till December with balance Rs.1664",
-    remark:"",
+    prevAdvance: 0,
+    prevBalance: 1664,
+    grandtotal: 3264,
+    paid: 1600,
+    paidOn: "15-01-2024",
+    nextBalance: 1664,
+    nextAdvance: 0,
+    status: "paid till December with balance Rs.1664",
+    remark: "",
 }
 
-const GetRentByRoomNoAndMonth = (async (req, res)=>{
-    const {roomNumber , billMonth} = req.body
-
+const GetRentByRoomNoAndMonth = (async (req, res) => {
+    const { roomNumber, billMonth } = req.body
     let query = { roomNumber: roomNumber };
 
-// If billMonth is provided, include it in the query
-if (billMonth !== undefined) {
-    query.billMonth = billMonth;
-}
-    const rent  = await RentModal.find(query)
-    res.json({data : rent})
+    // If billMonth is provided, include it in the query
+    if (billMonth !== undefined) {
+        query.billMonth = billMonth;
+    }
+    const rent = await RentModal.find(query)
+    console.log("rent", rent)
+    res.json({ data: rent })
 })
 
 
-const CreateRent = (async(req, res) => {
-    const {roomNumber , billMonth , start,end , unit  ,roomRent ,total ,prevAdvance,
-        prevBalance,grandtotal, paid, paidOn, nextBalance,nextAdvance,status} = req.body
-    try{
-        const date = moment(billMonth, "MMMM");
-        const previousMonth = date.subtract(1, 'months');
-        const previousMonthName = previousMonth.format('MMMM');
-        console.log(previousMonthName)
-        const PrevMonthRent = await RentModal.find({roomNumber : roomNumber , billMonth:previousMonthName})
-        const start =PrevMonthRent && PrevMonthRent.length > 0 && PrevMonthRent[0].end ? PrevMonthRent[0].end : 0
-   
-        console.log(PrevMonthRent , PrevMonthRent.length > 0 && PrevMonthRent[0].end)
-        const unit      = end - start
-        const totalBill = unit * 8
-       
+const CreateRent = (async (req, res) => {
+    const { id, remark, roomNumber, billMonth, start, end, unit, roomRent, total, prevAdvance,
+        prevBalance, grandtotal, paid, paidOn, nextBalance, nextAdvance, status } = req.body
+    try {
 
-        const total = roomRent + totalBill
-        const prevAdvance  = PrevMonthRent && PrevMonthRent.length > 0 && PrevMonthRent[0].nextAdvance ? PrevMonthRent[0].nextAdvance : 0
-        const prevBalance = PrevMonthRent && PrevMonthRent.length > 0 && PrevMonthRent[0].nextBalance ? PrevMonthRent[0].nextBalance : 0
-        const grandtotal = total - prevAdvance + prevBalance
-       
+        if (!id) {
 
-        console.log(paid , grandtotal )
-        const nextAdvance = (paid > grandtotal) ? paid - grandtotal : 0
-        const nextBalance = paid < grandtotal ? grandtotal - paid : 0 
-        
-    
-        const newRent = new RentModal({
-            roomNumber: roomNumber,
-            billMonth:billMonth,
-            start:start,
-            end:end,
-            unit: unit ,
-            totalBill:totalBill,
-            roomRent:roomRent,
-            total:total,
-            prevAdvance:prevAdvance,
-            prevBalance:prevBalance,
-            grandtotal:grandtotal,
-            paid:paid,
-            paidOn:paidOn,
-            nextBalance:nextBalance,
-            nextAdvance:nextAdvance,
-            status:`paid till ${billMonth} with ${nextAdvance > 0 ? `Advance Rs${nextAdvance}` : "" } ${nextBalance > 0 ? `Balance Rs${nextBalance}` : "" }` 
-        }) 
 
-        newRent.save().then(()=>{
-            res.status(200).json({ msg:"Rent created sucessfully" });
-        }).catch(()=>{
-            res.status(500).json({ msg:"Something went wrong" });
-        })
+            const date = moment(billMonth, "MMMM");
+            const previousMonth = date.subtract(1, 'months');
+            const previousMonthName = previousMonth.format('MMMM');
+            console.log(previousMonthName)
+            const PrevMonthRent = await RentModal.find({ roomNumber: roomNumber, billMonth: previousMonthName })
+            const start = PrevMonthRent && PrevMonthRent.length > 0 && PrevMonthRent[0].end ? PrevMonthRent[0].end : 0
+            console.log(PrevMonthRent, PrevMonthRent.length > 0 && PrevMonthRent[0].end)
+            const unit = end - start
+            const totalBill = unit * 8
+            const total = roomRent + totalBill
+            const prevAdvance = PrevMonthRent && PrevMonthRent.length > 0 && PrevMonthRent[0].nextAdvance ? PrevMonthRent[0].nextAdvance : 0
+            const prevBalance = PrevMonthRent && PrevMonthRent.length > 0 && PrevMonthRent[0].nextBalance ? PrevMonthRent[0].nextBalance : 0
+            const grandtotal = total - prevAdvance + prevBalance
+            console.log(paid, grandtotal)
+            const nextAdvance = (paid > grandtotal) ? paid - grandtotal : 0
+            const nextBalance = paid < grandtotal ? grandtotal - paid : 0
+            const newRent = new RentModal({
+                roomNumber: roomNumber,
+                billMonth: billMonth,
+                start: start,
+                end: end,
+                unit: unit,
+                totalBill: totalBill,
+                roomRent: roomRent,
+                total: total,
+                prevAdvance: prevAdvance,
+                prevBalance: prevBalance,
+                grandtotal: grandtotal,
+
+                nextBalance: nextBalance,
+                nextAdvance: nextAdvance,
+                status: `paid till ${billMonth} with ${nextAdvance > 0 ? `Advance Rs${nextAdvance}` : ""} ${nextBalance > 0 ? `Balance Rs${nextBalance}` : ""}`
+            })
+
+            newRent.save().then(() => {
+                res.status(200).json({ msg: "Rent created sucessfully" });
+            }).catch(() => {
+                res.status(500).json({ msg: "Something went wrong" });
+            })
+        }
+        else {
+             await RentModal.findByIdAndUpdate(id, {
+                paid: paid,
+                paidOn: paidOn,
+                remark: remark
+            }, (err, docs) => {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    res.status(200).json({ msg: "Rent Updated sucessfully" });
+                }
+            })
+
+        }
 
     }
-    catch(error){
+    catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 })
